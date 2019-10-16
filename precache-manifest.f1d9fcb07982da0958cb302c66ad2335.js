88c40fa302cb7f329a02414cd9fbc34fa22486fe
Sebastian Glaser <anx@ulzq.de>
2019-10-16 13:34:19 +0200

    Updates

---
+++ precache-manifest.f1d9fcb07982da0958cb302c66ad2335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc78334cdb30fc69d1a518350ddeced0",
+    "url": "/dci_cannon_fudder/index.html"
+  },
+  {
+    "revision": "59fc091fe978960f5a39",
+    "url": "/dci_cannon_fudder/static/css/main.234871f4.chunk.css"
+  },
+  {
+    "revision": "ce8d7496229f2f685d8a",
+    "url": "/dci_cannon_fudder/static/js/2.9ad1135c.chunk.js"
+  },
+  {
+    "revision": "59fc091fe978960f5a39",
+    "url": "/dci_cannon_fudder/static/js/main.dc1bb218.chunk.js"
+  },
+  {
+    "revision": "6ba36daf40df3415c181",
+    "url": "/dci_cannon_fudder/static/js/runtime-main.3b4f3a91.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dci_cannon_fudder/static/media/logo.25bf045c.svg"
+  }
+]);
